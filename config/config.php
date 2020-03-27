1ff04d8d432c662a0dc5776a7ba5083efabc000c
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-03-27 00:10:07 -0700

    Add configuration settings for storage URL/path

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
 <?php
 namespace GifGrabber;
 
+use Exception;
+
 class Config {
   public static function getDatabaseHost(): string
   {
@@ -20,5 +22,34 @@ class Config {
   public static function getDatabasePassword(): string
   {
     return '***REMOVED***';
+  }
+
+  public static function getStorageFolderName(): string
+  {
+    return 'storage';
+  }
+
+  public static function getStoragePath(): string
+  {
+    $path = realpath(sprintf('../public/%s/', self::getStorageFolderName()));
+    if($path === false)
+    {
+      throw new Exception('Storage folder not found.');
+    }
+    return $path;
+  }
+
+  public static function getBaseUrl(): string
+  {
+    return 'http://localhost:3000';
+  }
+
+  public static function getStorageUrl(): string
+  {
+    return sprintf(
+      '%s/%s',
+      self::getBaseUrl(),
+      self::getStorageFolderName()
+    );
   }
 }
