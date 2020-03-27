16abe4b6faa2a095a63e7a767abac3258a2ce16c
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-03-27 00:10:52 -0700

    Finish Category nad Gif models

--- models/category.php
+++ models/category.php
@@ -1,4 +1,26 @@
 <?php
 namespace GifGrabber;
 
-class Category extends Model {}
+use DateTime;
+
+class Category extends Model {
+  public static function getTableName(): string
+  {
+    return 'category';
+  }
+
+  public function getName(): string
+  {
+    return $this->getString('name');
+  }
+
+  public function getCreatedAt(): DateTime
+  {
+    return $this->getDateTime('created_at');
+  }
+
+  public function getUpdatedAt(): DateTime
+  {
+    return $this->getDateTime('updated_at');
+  }
+}
--- models/gif.php
+++ models/gif.php
@@ -1,4 +1,77 @@
 <?php
 namespace GifGrabber;
 
-class Gif extends Model {}
+use DateTime;
+
+class Gif extends Model {
+  /** @var Category|null */
+  private $category = null;
+  protected $alsoSerialize = [
+    'AssetUrl'
+  ];
+  protected $doNotSerialize = [
+    'file_type'
+  ];
+
+  public static function getTableName(): string
+  {
+    return 'gif';
+  }
+
+  public function getCreatedAt(): DateTime
+  {
+    return $this->getDateTime('created_at');
+  }
+
+  public function getUpdatedAt(): DateTime
+  {
+    return $this->getDateTime('updated_at');
+  }
+
+  public function getApproved(): bool
+  {
+    return $this->getBool('approved');
+  }
+
+  public function getTitle(): string
+  {
+    return $this->getString('title');
+  }
+
+  public function getCaption(): string
+  {
+    return $this->getString('caption');
+  }
+
+  public function getUrl(): string
+  {
+    return $this->getString('url');
+  }
+
+  public function getFileType(): string
+  {
+    return $this->getString('file_type');
+  }
+
+  public function getCategoryId(): int
+  {
+    return $this->getInteger('category_id');
+  }
+
+  public function getCategory(): Category
+  {
+    if(is_null($this->category)){
+      $this->category = Category::findById($this->getCategoryId());
+    }
+    return $this->category;
+  }
+
+  public function getAssetUrl(): string
+  {
+    return sprintf(
+      '%s/gif/%d/animation.gif',
+      Config::getStorageUrl(),
+      $this->getId()
+    );
+  }
+}
