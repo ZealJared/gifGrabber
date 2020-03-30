4053b9e3b28291ea39c4e5328ef125c03718b24d
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-03-30 09:35:29 -0700

    Add categoryList home route/view

---
+++ ui/src/views/categoryList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(index, category) in categoryList" :key="index">{{ category.Name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      categoryList: []
+    }
+  },
+  async mounted () {
+    this.categoryList = await this.$api.getCategoryList()
+  }
+}
+</script>
