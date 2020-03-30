e472842c45b4279540df4a41c5929d243d2af3e7
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-03-30 09:34:17 -0700

    Add API component

---
+++ ui/src/api.js
@@ -0,0 +1,40 @@
+export default class Api {
+  constructor (router, store) {
+    this.baseUrl = 'http://localhost:3000'
+    this.router = router
+    this.store = store
+  }
+
+  async request (url, method = 'GET', body = null) {
+    const options = {
+      method: method
+    }
+    if (body) {
+      options.body = body
+    }
+    const response = await window.fetch(`${this.baseUrl}/${url}`, options)
+    const text = await response.text()
+    let object = null
+    try {
+      object = JSON.parse(text)
+    } catch (e) {
+      object = null
+    }
+    if (object === null) {
+      throw new Error(text)
+    }
+    return object
+  }
+
+  getRequest (url) {
+    return this.request(url, 'GET')
+  }
+
+  postRequest (url, body) {
+    return this.request(url, 'POST', body)
+  }
+
+  getCategoryList () {
+    return this.getRequest('category')
+  }
+}
--- ui/src/main.js
+++ ui/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Api from './api'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = new Api(router, store)
 
 new Vue({
   router,
