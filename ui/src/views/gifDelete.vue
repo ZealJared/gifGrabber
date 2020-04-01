3cd8734ba5fdad819bf4bb0108c56b708cf3ec34
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-04-01 14:42:15 -0700

    Add component to handle gif deletion

---
+++ ui/src/views/gifDelete.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert alert-danger">Deleting GIF...</div>
+</template>
+
+<script>
+export default {
+  async mounted () {
+    const gifResponse = await this.$api.gifDelete(this.$route.params.gifId)
+    if (gifResponse.data.gif) {
+      this.$router.replace({ name: 'categoryGifList', params: { categoryId: gifResponse.data.gif.CategoryId } })
+    } else {
+      throw new Error(gifResponse.errors[0])
+    }
+  }
+}
+</script>
