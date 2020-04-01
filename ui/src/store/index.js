d11015014b127e2235659c35a2ceca72ff973348
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-04-01 14:41:40 -0700

    Update store for global flash messages

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -5,8 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    errors: []
+  },
+  getters: {
+    errors (state) {
+      return state.errors || []
+    }
   },
   mutations: {
+    setErrors (state, payload) {
+      if (Array.isArray(payload)) {
+        state.errors = payload
+      }
+    }
   },
   actions: {
   },
