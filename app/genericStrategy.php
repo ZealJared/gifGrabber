3283622a9f67c149061af3825de74e6229a9650c
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-04-03 03:52:18 -0700

    Don't wait for massive image conversion

--- app/genericStrategy.php
+++ app/genericStrategy.php
@@ -121,7 +121,7 @@ class GenericStrategy extends Strategy
         $destinationPng = sprintf('%s/image.png', $this->getGif()->getStoragePath());
         copy($imageUrl, $destinationPng);
         exec(sprintf(
-          'mogrify -monitor -background white -quality 75 -resize 15000x15000\> %s %s',
+          'mogrify -monitor -background white -quality 75 -resize 15000x15000\> %s %s > /dev/null 2>/dev/null &',
           $destinationPng,
           $destinationJpg
         ));
