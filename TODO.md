37c9a7f0bd949750a8dec660b0f16c321c3788af
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2020-04-02 23:05:03 -0700

    Add random gif button

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 * Link back to gif category from Gif view page ✓
-* 'Random' button in category list to show random gif from category
+* 'Random' button in category list to show random gif from category ✓
 
 # Create a strategy for each site in existing database GIFs
 
--- ui/src/views/categoryGifList.vue
+++ ui/src/views/categoryGifList.vue
@@ -1,24 +1,43 @@
 <template>
-  <ul class="list-group">
-    <li class="list-group-item" v-for="(gif, index) in gifList" :key="index">
-      <router-link :to="{ name: 'gifView', params: { gifId: gif.Id } }">{{ gif.Title }}{{ !gif.Approved ? ' (unapproved)' : '' }}</router-link>
-    </li>
-  </ul>
+  <div>
+    <h1 v-if="category">{{ category.Name }}</h1>
+    <ul v-if="gifList.length > 0" class="list-group">
+      <li class="list-group-item">
+        <router-link class="btn btn-primary" :to="{ name: 'gifView', params: { gifId: randomGifId() } }">Random</router-link>
+      </li>
+      <li class="list-group-item" v-for="(gif, index) in gifList" :key="index">
+        <router-link :to="{ name: 'gifView', params: { gifId: gif.Id } }">{{ gif.Title }}{{ !gif.Approved ? ' (unapproved)' : '' }}</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      category: null,
       gifList: []
     }
   },
+  methods: {
+    randomGifId () {
+      const index = Math.floor(Math.random() * this.gifList.length)
+      return this.gifList[index].Id
+    }
+  },
   async mounted () {
-    const response = await this.$api.getGifList(this.$route.params.categoryId)
-    if (response.data.gif) {
-      this.gifList = response.data.gif
+    const gifResponse = await this.$api.getGifList(this.$route.params.categoryId)
+    if (gifResponse.data.gif) {
+      this.gifList = gifResponse.data.gif
     } else {
       throw new Error('No gif list returned.')
+    }
+    const categoryResponse = await this.$api.getCategory(this.$route.params.categoryId)
+    if (categoryResponse.data.category) {
+      this.category = categoryResponse.data.category
+    } else {
+      throw new Error('No category returned.')
     }
   }
 }
