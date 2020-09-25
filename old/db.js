96c278cdec34bb69d95d3817b4b62478a581ed45
Zeal Mayfield <zeal.j.mayfield@gmail.com>
2020-09-25 10:07:56 -0700

    Remove original reference version

--- old/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize")
-
-const sequelize = new Sequelize("gifgrabber", "gifgrabber", "***REMOVED***", {
-    dialect: "mysql"
-})
-
-module.exports = sequelize
--- old/url.js
+++
@@ -1,7 +0,0 @@
-const Url = {
-    encode: (string) => {
-        return string.replace(/[^A-z0-9]+/g, "+").toLowerCase()
-    }
-}
-
-module.exports = Url
