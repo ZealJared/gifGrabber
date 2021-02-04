d98b3efd940ba05c2d7c291e8e0560c2194cff7e
Zeal Mayfield <zeal.j.mayfield@gmail.com>
2021-02-04 15:25:24 -0800

    delete paths before writing to them update regex to match meta with more attrs

--- strategies/imgurStrategy.php
+++ strategies/imgurStrategy.php
@@ -12,28 +12,39 @@ class ImgurStrategy extends Strategy
   protected function saveVideo(): void
   {
     $matches = [];
-    preg_match('~property="og:video"\s+content="([^"]+?\.mp4)"~', $this->getPageContent(), $matches);
+    preg_match('~property="og:video"[\s\w\-="]+?content="([^"]+?\.mp4)"~', $this->getPageContent(), $matches);
     if (!empty($matches[1])) {
       $videoUrl = $matches[1];
     } else {
-      preg_match('~<meta name="twitter:player:stream"\s+content="([^"]+\.mp4)" />~', $this->getPageContent(), $matches);
+      preg_match('~<meta name="twitter:player:stream"[\s\w\-="]+?content="([^"]+\.mp4)"~', $this->getPageContent(), $matches);
       $videoUrl = $matches[1];
     }
-    copy($videoUrl, sprintf('%s/video.mp4', $this->getGif()->getStoragePath()));
+    $destination = sprintf('%s/video.mp4', $this->getGif()->getStoragePath());
+    if(file_exists($destination)){
+      unlink($destination);
+    }
+    copy($videoUrl, $destination);
   }
 
   protected function saveImage(): void
   {
     $matches = [];
-    preg_match('~<meta name="twitter:image"\s+content="([^"]+\.jpg)" />~', $this->getPageContent(), $matches);
+    preg_match('~<meta name="twitter:image"[\s\w\-="]+?content="([^"]+\.jpg)~', $this->getPageContent(), $matches);
     $imageUrl = $matches[1];
-    copy($imageUrl, sprintf('%s/image.jpg', $this->getGif()->getStoragePath()));
+    $destination = sprintf('%s/image.jpg', $this->getGif()->getStoragePath());
+    if(file_exists($destination)){
+      unlink($destination);
+    }
+    copy($imageUrl, $destination);
   }
 
   protected function saveGif(): void
   {
     $videoPath = sprintf('%s/video.mp4', $this->getGif()->getStoragePath());
     $animationPath = sprintf('%s/animation.gif', $this->getGif()->getStoragePath());
+    if(file_exists($animationPath)){
+      unlink($animationPath);
+    }
     exec(sprintf(
       'ffmpeg -i %s -vf "fps=12,scale=320:-1" -loop 0 %s',
       $videoPath,
