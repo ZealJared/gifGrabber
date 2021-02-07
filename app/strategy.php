3bf4f8f8f0c11f2834fbed9a82bd18d09a41d22b
Zeal Mayfield <zeal.j.mayfield@gmail.com>
2021-02-07 09:46:46 -0800

    Move normalization logic to Strategy

--- app/strategy.php
+++ app/strategy.php
@@ -8,12 +8,20 @@ abstract class Strategy {
   private $gif = null;
   /** @var string|null */
   private $pageContent = null;
+  protected $videoSaved = false;
+  protected $imageSaved = false;
+  protected $animationSaved = false;
 
   protected function matches(Gif $gif): bool {
     return preg_match($this->getPattern(), $gif->getUrl()) === 1;
   }
 
-  protected function getGif(): Gif
+  protected function getUrl(): string
+  {
+    return trim($this->getGif()->getUrl(), '/');
+  }
+
+  private function getGif(): Gif
   {
     if (is_null($this->gif)) {
       throw new Exception('Strategy Gif property is null.');
@@ -24,7 +32,12 @@ abstract class Strategy {
   protected function getPageContent(): string
   {
     if (is_null($this->pageContent)) {
-      $pageContent = file_get_contents($this->getGif()->getUrl());
+      $context = stream_context_create([
+        'http' => [
+          'header' => 'Cookie: over18=1\r\n'
+        ]
+      ]);
+      $pageContent = file_get_contents($this->getGif()->getUrl(), context: $context);
       if ($pageContent === false) {
         throw new Exception(sprintf('Unable to load URL: %s', $this->getGif()->getUrl()));
       }
@@ -35,11 +48,80 @@ abstract class Strategy {
 
   abstract protected function getPattern(): string;
 
-  abstract protected function saveImage(): void;
+  abstract protected function fetchImage(): void;
 
-  abstract protected function saveGif(): void;
+  abstract protected function fetchAnimation(): void;
 
-  abstract protected function saveVideo(): void;
+  abstract protected function fetchVideo(): void;
+
+  protected function saveImage(string $imageUrl): void
+  {
+    // error_log(sprintf('Saving image: %s', $imageUrl));
+    $this->getGif()->saveImage($imageUrl);
+    $this->imageSaved = true;
+  }
+
+  protected function saveVideo(string $videoUrl): void
+  {
+    // error_log(sprintf('Saving video: %s', $videoUrl));
+    $this->getGif()->saveVideo($videoUrl);
+    $this->videoSaved = true;
+  }
+
+  protected function saveAnimation(string $animationUrl): void
+  {
+    // error_log(sprintf('Saving animation: %s', $animationUrl));
+    $this->getGif()->saveAnimation($animationUrl);
+    $this->animationSaved = true;
+  }
+
+  protected function videoToAnimation(): void
+  {
+    // error_log('Converting video to animation.');
+    $this->getGif()->videoToAnimation();
+    $this->animationSaved = true;
+  }
+
+  protected function animationToVideo(): void
+  {
+    // error_log('Converting animation to video.');
+    $this->getGif()->animationToVideo();
+    $this->videoSaved = true;
+  }
+
+  protected function videoToImage(): void
+  {
+    // error_log('Getting image from video.');
+    $this->getGif()->videoToImage();
+    $this->imageSaved = true;
+  }
+
+  protected function animationToImage(): void
+  {
+    // error_log('Getting image from animation.');
+    $this->getGif()->animationToImage();
+    $this->imageSaved = true;
+  }
+
+  protected function normalize(): void
+  {
+    // if video is saved and animation is not, video to animation
+    if($this->videoSaved && !$this->animationSaved){
+      $this->videoToAnimation();
+    }
+    // if video is saved and image is not, video to image
+    if($this->videoSaved && !$this->imageSaved){
+      $this->videoToImage();
+    }
+    // if animation is saved and video is not and image is not, animation to image
+    if($this->animationSaved && !$this->videoSaved && !$this->imageSaved){
+      $this->animationToImage();
+    }
+    // if animation is saved and video is not, animation to video
+    if($this->animationSaved && !$this->videoSaved){
+      $this->animationToVideo();
+    }
+  }
 
   public function execute(Gif $gif): bool
   {
@@ -47,9 +129,10 @@ abstract class Strategy {
       return false;
     }
     $this->gif = $gif;
-    $this->saveVideo();
-    $this->saveImage();
-    $this->saveGif();
+    $this->fetchVideo();
+    $this->fetchAnimation();
+    $this->fetchImage();
+    $this->normalize();
     return true;
   }
 }
