185cb9c5fc3973d9e728f9e43888ed633dc38924
Zeal Mayfield <zeal.j.mayfield@gmail.com>
2021-02-07 09:30:12 -0800

    Utility to remove query and fragment from URL

--- app/utility.php
+++ app/utility.php
@@ -2,5 +2,35 @@
 namespace GifGrabber;
 
 class Utility {
-  
+  public static function getUrlFileExtension(string $url): string
+  {
+    return strtolower(pathinfo(self::getUrlPath($url), PATHINFO_EXTENSION));
+  }
+
+  public static function getUrlPath(string $url): string
+  {
+    $urlString = '';
+    $urlParts = parse_url($url);
+    $scheme = $urlParts['scheme'] ?? '';
+    if($scheme)
+    {
+      $urlString .= $scheme . '://';
+    }
+    $user = $urlParts['user'] ?? '';
+    $pass = $urlParts['pass'] ?? '';
+    if($user && $pass)
+    {
+      $urlString .= $user . ':' . $pass . '@';
+    }
+    $host = $urlParts['host'] ?? '';
+    $urlString .= $host;
+    $port = $urlParts['port'] ?? '';
+    if($port)
+    {
+      $urlString .= ':' . $port;
+    }
+    $path = $urlParts['path'] ?? '';
+    $urlString .= $path;
+    return $urlString;
+  }
 }
